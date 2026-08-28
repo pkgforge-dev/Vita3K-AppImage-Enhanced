0df029209be6c95fa28ffa06e5f053bf3e7e1421
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-28 15:38:33 -0400

    deploy pulseaudio

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,7 @@ export ICON=/usr/share/icons/hicolor/scalable/apps/org.vita3k.vita3k.svg
 export DESKTOP=/usr/share/applications/org.vita3k.vita3k.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
+export DEPLOY_PULSE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/Vita3K* /usr/share/Vita3K
